9442a2a7618e4a15f0d9b4e27e82daec501f8e2f
blankuet <blancafeijoocruz@gmail.com>
2024-04-16 21:15:29 +0200

    solved lab

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,60 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+      this.currentTime = 0;
+      this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    if (this.intervalId === null) {
+      this.intervalId = setInterval(() => {
+        this.currentTime++;
+        if (printTimeCallback){
+          printTimeCallback(this.currentTime);
+        }
+        /* printTimeCallback(); */
+      }, 1000);
+    }
   }
 
   getMinutes() {
-    // ... your code goes here
+    if (this.currentTime === 0){
+      return 0;
+    } else {
+     return Math.floor(this.currentTime / 60); 
+    }
+    
   }
 
   getSeconds() {
-    // ... your code goes here
+    if (this.currentTime === 0) {
+      return 0;
+    } else {
+      return this.currentTime % 60;
+    }
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value < 10) {
+      // value = 10;
+    return `0${value}`;
+    } else {
+      // value = 2;
+    return `${value}`;
+    }  
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    const minutes = this.computeTwoDigitNumber(this.getMinutes());
+    const seconds = this.getSeconds();
+    const computedSeconds = this.computeTwoDigitNumber(seconds);
+    return `${minutes}:${computedSeconds}`;
   }
 }
